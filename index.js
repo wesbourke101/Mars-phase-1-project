4f42524511ee528a6e2ac805b8944195e3375325
wesley <wesbourkeavl@gmail.com>
2022-03-07 17:26:34 -0500

    img selector works using the calendar selector, merging to complete.

--- index.js
+++ index.js
@@ -2,17 +2,22 @@
 
 let date = "2015-03-03"
 const formLocation = document.querySelector('#date')
-const imageLocation = document.querySelector('fistImg')
-fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${date}`)
-.then((data) => data.json())
-.then((rovers) => {
-    console.log(rovers)
-    formLocation.addEventListener('input', (e, rovers) => {
+const imageLocation = document.querySelector('#fistImg')
+
+function findDate () {
+    formLocation.addEventListener('input', (e) => {
         e.preventDefault
         let test = formLocation.value
         console.log(test)
         fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${test}`)
         .then((data) => data.json())
-        .then((dataPic) => console.log(dataPic))
-    })
-})
+        //.then((dataPic) => console.log(dataPic.img_src))
+        .then((dataPic) => {
+           //console.log(dataPic.photos[0].img_src)
+            imageLocation.src = dataPic.photos[0].img_src
+        })
+        
+    })  
+}
+//Run all functions
+findDate()
