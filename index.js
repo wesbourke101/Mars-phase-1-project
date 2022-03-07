34596cfd1e9bed12fe7f4cb645b64d24e74c75eb
wesley <wesbourkeavl@gmail.com>
2022-03-07 16:55:31 -0500

    found single day location using calendar

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 // https://github.com/chrisccerami/mars-photo-api
 
-let dateInput =document.querySelector('#date').value
-const imgContainer = document.getElementsByClassName('imgContainer')
-
-let date ="2015-11-03"
-const calendarFormLocation = document.querySelector('#calendarDate')
-
-fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&earth_date=${dateInput}`)
+let date = "2015-03-03"
+const formLocation = document.querySelector('#date')
+const imageLocation = document.querySelector('fistImg')
+fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${date}`)
 .then((data) => data.json())
-.then((rovers) => getPicOfRover(rovers))
-
-
-//do we need this
-function getPicOfRover(rovers) {
-    const imgLocation = document.querySelector('.img')
-    imgLocation.src = rovers.photos[0].img_src
-}
-calendarFormLocation.addEventListener('input', callBackForm)
-
-function callBackForm(event) {
-    event.preventDefault();
-    //get the values
-    dateInput =document.querySelector('#date').value
-    console.log(event.target.value)
-    return dateInput
-    //date = dateInput
-    //format
-    //submit user input into date value
-}
+.then((rovers) => {
+    console.log(rovers)
+    formLocation.addEventListener('input', (e, rovers) => {
+        e.preventDefault
+        let test = formLocation.value
+        console.log(test)
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${test}`)
+        .then((data) => data.json())
+        .then((dataPic) => console.log(dataPic))
+    })
+})
