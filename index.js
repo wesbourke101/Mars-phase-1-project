0f2f5731c5dc7250f1f49af193c82091e9b2ae73
wesley <wesbourkeavl@gmail.com>
2022-03-08 00:06:08 -0500

    added a low functioning rover search. Search works, we need to set the limitations for date range. git push ' ''

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 // https://github.com/chrisccerami/mars-photo-api
-
 let date = "2015-03-03"
 const formLocation = document.querySelector('#date')
 const imageLocation = document.querySelector('#fistImg')
+const roverSelectionLocation = document.querySelector('#roverSelection')
 
 function findDate () {
     formLocation.addEventListener('input', (e) => {
         e.preventDefault
-        let test = formLocation.value
-        console.log(test)
-        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${test}`)
+        let setRover = roverSelectionLocation.value
+        let setDate = formLocation.value
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${setRover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setDate}`)
         .then((data) => data.json())
-        //.then((dataPic) => console.log(dataPic.img_src))
         .then((dataPic) => {
-           //console.log(dataPic.photos[0].img_src)
+            console.log(dataPic)
             imageLocation.src = dataPic.photos[0].img_src
         })
-        
+    })
+    roverSelectionLocation.addEventListener('submit', (e) => {
+        console.log('you made it here')
     })  
 }
 //Run all functions
