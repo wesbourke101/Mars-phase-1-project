3c8e9040789880fae30124448f2e1f2a82dab855
Sarah Indano <sarah.indano@gmail.com>
2022-03-08 15:02:04 -0500

    added css, formatted HTML, specified variables for curiosity working towards multiple cards

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
 // https://github.com/chrisccerami/mars-photo-api
+////////////////////Curiosity///////////////////////
+let curiosityDateLocation = document.querySelector('#curiosityDate')
+let curiosityImgLocation = document.querySelector(`#curiosityImg`)
+let curiositySelectionLocation = document.querySelector("#curiosityID").textContent
+let curiosityCameraSelection = document.querySelector("#curiosityCameraSelection")
 
-let dateValue = document.querySelector('#curiosityDate')
-let imageLocation = document.querySelector(`#curiosityImg`)
-let roverSelectionLocation = document.querySelector("#curiosityID").textContent
-let cameraSelection = document.querySelector("#curiosityCameraSelection")
+let setCuriosityRover = curiositySelectionLocation
+let setCuriosityDate = curiosityDateLocation.value
+let setCuriosityCamera = curiosityCameraSelection.value
 
-let setRover = roverSelectionLocation
-let setDate = dateValue.value
-let setCamera = cameraSelection.value
-
-dateValue.addEventListener('input', (e) => {
+curiosityDateLocation.addEventListener('input', (e) => {
     e.preventDefault()
-    setDate = dateValue.value
+    setCuriosityDate = curiosityDateLocation.value
     requestRoverView()
 })
 
-cameraSelection.addEventListener('change', (e) => {
+curiosityCameraSelection.addEventListener('change', (e) => {
     e.preventDefault()
-    setCamera = cameraSelection.value
+    setCuriosityCamera = curiosityCameraSelection.value
     requestRoverView()
 })
 
 function requestRoverView () {
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${setRover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setDate}&camera=${setCamera}`)
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${setCuriosityRover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setCuriosityDate}&camera=${setCuriosityCamera}`)
     .then((data) => data.json())
     .then((dataPic) => {
-        imageLocation.src = dataPic.photos[0].img_src
+        curiosityImgLocation.src = dataPic.photos[0].img_src
     }) 
 }
+
+////////////////////Spirit/////////////////////////
+
+// let spiritDateLocation = document.querySelector('#spiritDate')
+// let spiritImgLocation = document.querySelector(`#spiritImg`)
+// let spiritSelectionLocation = document.querySelector("#spiritID").textContent
+// let spiritCameraSelection = document.querySelector("#spiritCameraSelection")
+
+// let setSpiritRover = spiritSelectionLocation
+// let setSpiritDate = spiritDateLocation.value
+// let setSpiritCamera = spiritCameraSelection.value
+
+// spiritDateLocation.addEventListener('input', (e) => {
+//     e.preventDefault()
+//     setSpiritDate = spiritDateLocation.value
+//     requestRoverView()
+// })
+
+// spiritCameraSelection.addEventListener('change', (e) => {
+//     e.preventDefault()
+//     setSpiritCamera = spiritCameraSelection.value
+//     requestRoverView()
+// })
+
+// function requestRoverView () {
+//     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${setSpiritRover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setSpiritDate}&camera=${setSpiritCamera}`)
+//     .then((data) => data.json())
+//     .then((dataPic) => {
+//         spiritImgLocation.src = dataPic.photos[0].img_src
+//     }) 
+// }
