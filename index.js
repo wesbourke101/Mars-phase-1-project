c590864ccba323db5165aa69bcf8e671029bc2b2
Sarah Indano <sarah.indano@gmail.com>
2022-03-08 11:26:20 -0500

    html: added containers for each card. Experimenting with creating cards in JS

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 // https://github.com/chrisccerami/mars-photo-api
-let date = "2015-03-03"
-const formLocation = document.querySelector('#date')
-const imageLocation = document.querySelector('#fistImg')
-const roverSelectionLocation = document.querySelector('#roverSelection')
+
+let dateValue = document.querySelector('#curiosityDate')
+let formLocation = document.querySelector('#calendarDate')
+let imageLocation = document.querySelector(`#curiosityImg`)
+let roverSelectionLocation = document.querySelector("#curiosityID").textContent
+let cameraSelection = document.querySelector("#curiosityCameraSelection")
+
+
 
 function findDate () {
-    formLocation.addEventListener('input', (e) => {
-        e.preventDefault
-        let setRover = roverSelectionLocation.value
-        let setDate = formLocation.value
-        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${setRover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setDate}`)
+    dateValue.addEventListener('input', (e) => {
+            e.preventDefault
+            let setRover = roverSelectionLocation
+            let setDate = dateValue.value
+            let setCamera = cameraSelection.value
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${setRover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setDate}&camera=${setCamera}`)
         .then((data) => data.json())
         .then((dataPic) => {
             console.log(dataPic)
             imageLocation.src = dataPic.photos[0].img_src
+        })  
+        cameraSelection.addEventListener('change', (e) => {
+            console.log('camera selectionValue after click:', cameraSelection.value)
         })
     })
-    roverSelectionLocation.addEventListener('submit', (e) => {
-        console.log('you made it here')
-    })  
 }
-//Run all functions
+
 findDate()
