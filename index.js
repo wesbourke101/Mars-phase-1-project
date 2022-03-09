24ade7eab07ac3f33f741ca71be23c4794c906c9
Sarah Indano <sarah.indano@gmail.com>
2022-03-09 17:38:46 -0500

    cleaned up JS, reformatted CSS, added h1 to HTML

--- index.js
+++ index.js
@@ -1,103 +1,108 @@
 document.addEventListener("DOMContentLoaded", function(){
     const marsRoverContainer = document.querySelector('.roverContainer')
-    const mars_rovers = ['curiosity', 'perseverance', 'opportunity', 'spirit'];
-    // create default card
-        //add eventlisterns to cards
-        //add all ablities the card will need
+    const mars_rovers = ['perseverance', 'curiosity', 'opportunity', 'spirit'];
+
+
     mars_rovers.forEach(rover => {
-        const cameraRoverArray =[['FHAZ', 'RHAZ', 'NAVCAM'],['NAVCAM_LEFT', 'NAVCAM_RIGHT', 'MCZ_LEFT', 'MCZ_RIGHT'],['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM'], ['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM']]
+        const cameraRoverArray =[['NAVCAM_LEFT', 'NAVCAM_RIGHT', 'MCZ_LEFT', 'MCZ_RIGHT'],['FHAZ', 'RHAZ', 'NAVCAM'],['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM'], ['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM']]
         
         const roverDiv = document.createElement('div')
-        roverDiv.className = `rover-class`
-        roverDiv.id = `rover-${rover}-id`
+            roverDiv.className = `rover-class`
+            roverDiv.id = `rover-${rover}-id`
         
-        const roverH2 = document.createElement('h2')
-        roverH2.id =`${rover}-h2`
-        roverH2.textContent = `${rover}` //need to uppercase 1st letter
+            const roverH2 = document.createElement('h2')
+                roverH2.id =`${rover}-h2`
+                roverH2.textContent = `${rover}`
 
         ///////////////////////////////////
-        const roverForm = document.createElement('form')
-        roverForm.id = `${rover}-form`
-        roverForm.className = `form`
-        const roverFormDateInput = document.createElement('input')
-        roverFormDateInput.id =`${rover}-date`
-        roverFormDateInput.type ="date"
-        roverFormDateInput.className ="dateInput"
+            const roverForm = document.createElement('form')
+                roverForm.id = `${rover}-form`
+                roverForm.className = `form`
         ///////////////////////////////////
-        const roverCameraSelect = document.createElement('select')
-        roverCameraSelect.id = `${rover}-cameraSelect`
-        roverCameraSelect.className =`cameraSelection`
-        if (rover === "curiosity"){
-            for(let camera of cameraRoverArray[0]) {
-                const roverOption = document.createElement('option')
-                roverOption.value = camera
-                roverOption.textContent =camera
-                roverCameraSelect.append(roverOption)
-            }
-        } else if (rover === "perseverance") {
-            for(let camera of cameraRoverArray[1]) {
-                const roverOption = document.createElement('option')
-                roverOption.value = camera
-                roverOption.textContent =camera
-                roverCameraSelect.append(roverOption)
-            }           
-        } else if (rover === "opportunity") {
-            for(let camera of cameraRoverArray[2]) {
-                const roverOption = document.createElement('option')
-                roverOption.value = camera
-                roverOption.textContent =camera
-                roverCameraSelect.append(roverOption)
-            }
-        } else if (rover === "spirit") {
-            for(let camera of cameraRoverArray[3]) {
-                const roverOption = document.createElement('option')
-                roverOption.value = camera
-                roverOption.textContent =camera
-                roverCameraSelect.append(roverOption)
-            }
-        }
-        console.log(roverH2)
+                const roverFormDateInput = document.createElement('input')
+                    roverFormDateInput.id =`${rover}-date`
+                    roverFormDateInput.type ="date"
+                    roverFormDateInput.className ="dateInput"
+                    if (rover === "perseverance"){
+                        roverFormDateInput.min = "2021-02-18"
+                    } else if (rover === "curiosity") {
+                        roverFormDateInput.min = "2012-08-06"
+                    } else if (rover === "opportunity") {
+                        roverFormDateInput.min = "2004-01-25"
+                        roverFormDateInput.max = "2018-06-10"
+                    } else if (rover === "spirit") {
+                        roverFormDateInput.min = "2004-01-04"
+                        roverFormDateInput.max = "2010-03-22"
+                    }
         ///////////////////////////////////
-        const formSubmitBTN = document.createElement('button')
-        formSubmitBTN.className =`submitBTN`
-        formSubmitBTN.textContent = "Submit"
-        formSubmitBTN.addEventListener("click", function (e) {
-            e.preventDefault()
-            console.log('button eventlistener')
-            const webImgLocation = document.querySelector(`#${rover}Img`)
-            console.log(rover)
-            const roverDate = document.querySelector(`#${rover}-date`)
-            const cameraLocation = document.querySelector(`#${rover}-cameraSelect`)
-            let setDate = roverDate.value
-            let setCamera = cameraLocation.value
-            
-            fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setDate}&camera=${setCamera}`)
-            .then((data) => data.json())
-            .then((dataPic) => {
-                if (dataPic.photos.length > 0) {
-                    webImgLocation.src = dataPic.photos[0].img_src
-                } else {
-                    webImgLocation.src = "https://www.wired.com/images_blogs/wiredscience/2012/08/curiosoitycameras2.jpg"
-                }
-            }) 
-            //roverForm.reset()
-        })
-        roverForm.append(roverFormDateInput, roverCameraSelect, formSubmitBTN)
+                const roverCameraSelect = document.createElement('select')
+                    roverCameraSelect.id = `${rover}-cameraSelect`
+                    roverCameraSelect.className =`cameraSelection`
+                    if (rover === "perseverance"){
+                        for(let camera of cameraRoverArray[0]) {
+                            const roverOption = document.createElement('option')
+                                roverOption.value = camera
+                                roverOption.textContent =camera
+                            roverCameraSelect.append(roverOption)
+                        }
+                    } else if (rover === "curiosity") {
+                        for(let camera of cameraRoverArray[1]) {
+                            const roverOption = document.createElement('option')
+                                roverOption.value = camera
+                                roverOption.textContent =camera
+                            roverCameraSelect.append(roverOption)
+                        }           
+                    } else if (rover === "opportunity") {
+                        for(let camera of cameraRoverArray[2]) {
+                            const roverOption = document.createElement('option')
+                                roverOption.value = camera
+                                roverOption.textContent =camera
+                            roverCameraSelect.append(roverOption)
+                        }
+                    } else if (rover === "spirit") {
+                        for(let camera of cameraRoverArray[3]) {
+                            const roverOption = document.createElement('option')
+                                roverOption.value = camera
+                                roverOption.textContent =camera
+                            roverCameraSelect.append(roverOption)
+                        }
+                    }
         ///////////////////////////////////
-        const imgLocation = document.createElement('img')
-        imgLocation.className ='img'
-        imgLocation.id =`${rover}Img`
-        imgLocation.src = 'https://spaceplace.nasa.gov/mars-rovers/en/mars-rovers_metal-plate.en.jpg'
+                const formSubmitBTN = document.createElement('button')
+                    formSubmitBTN.className =`submitBTN`
+                    formSubmitBTN.textContent = "Submit"
+                formSubmitBTN.addEventListener("click", function (e) {
+                    e.preventDefault()
 
-        const paragraphTag = document.createElement('p')
-        paragraphTag.className ="paragraph"
-        paragraphTag.textContent = "place holder text"
+                    const webImgLocation = document.querySelector(`#${rover}Img`)
+                    const roverDate = document.querySelector(`#${rover}-date`)
+                    const cameraLocation = document.querySelector(`#${rover}-cameraSelect`)
+
+                    let setDate = roverDate.value
+                    let setCamera = cameraLocation.value
+                    
+                    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?api_key=JHdB5Rdk7d8gcc7HSTXEAjo3P7d2dMNk44p6HVfu&earth_date=${setDate}&camera=${setCamera}`)
+                    .then((data) => data.json())
+                    .then((dataPic) => {
+                        if (dataPic.photos.length > 0) {
+                            webImgLocation.src = dataPic.photos[0].img_src
+                        } else {
+                            webImgLocation.src = "https://c8.alamy.com/zooms/9/f68a5370d7aa46af9d5c89f3378e9964/2a14gb4.jpg"
+                        }
+                    }) 
+                })
+            roverForm.append(roverFormDateInput, roverCameraSelect, formSubmitBTN)
+        ///////////////////////////////////
+            const imgLocation = document.createElement('img')
+                imgLocation.className ='img'
+                imgLocation.id =`${rover}Img`
+                imgLocation.src = 'https://spaceplace.nasa.gov/mars-rovers/en/mars-rovers_metal-plate.en.jpg'
+
+            const paragraphTag = document.createElement('p')
+                paragraphTag.className ="paragraph"
+                paragraphTag.textContent = "place holder text"
 
         roverDiv.append(roverH2, roverForm, imgLocation, paragraphTag)
-        console.log(roverDiv)
-        marsRoverContainer.append(roverDiv)
+    marsRoverContainer.append(roverDiv)
     }); 
-    //what rover - comes from an H2- locate this- change this
-    //what day - come from form- locate this- change this
-    //what camera - come from form- locate this- change this
 })
