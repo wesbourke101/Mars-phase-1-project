79aec0af45d3a9c20a793bd21bbd82fea3443900
wesley <wesbourkeavl@gmail.com>
2022-03-10 13:34:48 -0500

    Added alt text to img's, added paragraphs, changed headers, added css nasa font.

--- index.js
+++ index.js
@@ -86,8 +86,10 @@ document.addEventListener("DOMContentLoaded", function(){
                     .then((dataPic) => {
                         if (dataPic.photos.length > 0) {
                             webImgLocation.src = dataPic.photos[0].img_src
+                            webImgLocation.alt =`Image from ${setCamera} on ${rover} on ${setDate}`       
                         } else {
                             webImgLocation.src = "https://c8.alamy.com/zooms/9/f68a5370d7aa46af9d5c89f3378e9964/2a14gb4.jpg"
+                            webImgLocation.alt =`Image from ${setCamera} on ${rover} on ${setDate} not found!`                             
                         }
                     }) 
                 })
@@ -97,10 +99,21 @@ document.addEventListener("DOMContentLoaded", function(){
                 imgLocation.className ='img'
                 imgLocation.id =`${rover}Img`
                 imgLocation.src = 'https://spaceplace.nasa.gov/mars-rovers/en/mars-rovers_metal-plate.en.jpg'
+                imgLocation.alt ="Mars Rovers sillhouette"
 
             const paragraphTag = document.createElement('p')
                 paragraphTag.className ="paragraph"
-                paragraphTag.textContent = "place holder text"
+                if (rover === "perseverance"){
+                    paragraphTag.innerHTML = '<strong>Launch Date:</strong> July 30, 2020, Cape Canaveral Air Force Station, Florida <br><strong>Date Landed:</strong> Feb. 18, 2021 <br><strong>Mission Status:</strong> Active <br><strong>Mission Name:</strong> Mars 2020 <br><strong>Mission Goals:</strong> The Perseverance rover will seek signs of ancient life and collect rock and soil samples for possible return to Earth. <br><strong>Mission Duration:</strong> At least one Mars year (roughly 687 Earth days) Where Perseverance Traveled: 4.49 KM from the Jezero crater, Mars.'             
+                } else if (rover === "curiosity") {
+                    paragraphTag.innerHTML = '<strong>Launch Date:</strong> 26 November 2011, 15:02 UTC from Cape Canaveral <br><strong>Mission Name:</strong> Mars Science Laboratory<br><strong>Mission Goals:</strong> To contribute to the four science goals and meet its specific goal of determining Mars habitability, Mars Science Laboratory has the following science objectives.<br><strong>Where Curiosity Traveled:</strong> 27.14 km (16.86 mi) on Mars as of 9 February 2022'
+                } else if (rover === "opportunity") {
+                    paragraphTag.innerHTML = "<strong>Launch Date:</strong> July 8, 2003 UTC<br><strong>Date Landed:</strong> January 25, 2004<br><strong>Mission Status:</strong> Retired<br><strong>Completed:</strong> February 13, 2019<br><strong>Mission Goals:</strong> determine whether life as we know it could ever have arisen on Mar(focusing particularly on searching for ancient water) and characterizing the climate and geology of Mars.<br><strong>Where Opportunity Traveled:</strong> 45.16 km (28.06 mi) from Eagle, Meridiani Planum"
+                } else if (rover === "spirit") {
+                    paragraphTag.innerHTML = "<strong>Launch Date:</strong> June 10, 2003 UTC<br><strong>Date Landed:</strong> January 4, 2004 Mission Status:<br><strong>Completed:</strong> Retired, February 13, 2019<br><strong>Mission Goals:</strong> determine whether life as we know it could ever have arisen on Mars (focusing particularly on searching for ancient water) and characterizing the climate and geology of Mars.<br><strong>Where Spirit Traveled:</strong> 7.73 km (4.8 mi) from the GPS position 14.5684°S 175.472636°E"
+                }
+
+
 
         roverDiv.append(roverH2, roverForm, imgLocation, paragraphTag)
     marsRoverContainer.append(roverDiv)
